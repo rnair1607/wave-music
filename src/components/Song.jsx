9a746dcc9c212cd16c808494c7731749e9a6799a
Rahul Nair <rnair1607@gmail.com>
2020-12-02 23:38:28 +0530

    initial commit

---
+++ src/components/Song.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Song = (props) => {
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h1>Song name</h1>
+      <h1>Artist</h1>
+    </div>
+  );
+};
+
+export default Song;
