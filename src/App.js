42aeb55803c45e614fcd35a759b85761306b30fd
Rahul Nair <rnair1607@gmail.com>
2020-12-03 22:44:53 +0530

    something

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
 
---
+++ src/util.js
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Beaver Creek",
+      cover: "",
+    },
+  ];
+}
